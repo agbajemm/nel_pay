0860916f487ec2c1eea54c3b4bc47f04df24d687
agbajemm <agbajemhmd316@gmail.com>
2022-08-07 23:50:07 +0100

    initial

---
+++ src/main/java/com/example/nel_pay/NelPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.nel_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nel_pay/NelPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nel_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

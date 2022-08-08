7e46e2fe4f85495a81811e2098e7855601422752
agbajemm <agbajemhmd316@gmail.com>
2022-08-09 00:12:17 +0100

    reg

--- src/main/java/com/example/nel_pay/registration/RegistrationService.java
+++ src/main/java/com/example/nel_pay/registration/RegistrationService.java
@@ -1,17 +1,30 @@
 package com.example.nel_pay.registration;
 
+import com.example.nel_pay.appuser.AppUser;
+import com.example.nel_pay.appuser.AppUserRole;
+import com.example.nel_pay.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
     private EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
         boolean isValidEmail = emailValidator.test(request.getEmail());
                 if(!isValidEmail){
                     throw new IllegalStateException("email not valid");
                 }
-        return "works";
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+
+                )
+        );
     }
 }

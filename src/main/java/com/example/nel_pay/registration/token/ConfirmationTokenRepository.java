600c43a4f7a3d8ab8a515c617644ea604388c5bd
agbajemm <agbajemhmd316@gmail.com>
2022-08-15 13:19:18 +0100

    confirmation

--- src/main/java/com/example/nel_pay/registration/token/ConfirmationTokenRepository.java
+++ src/main/java/com/example/nel_pay/registration/token/ConfirmationTokenRepository.java
@@ -1,4 +1,12 @@
 package com.example.nel_pay.registration.token;
 
-public interface ConfirmationTokenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
 }
--- src/main/java/com/example/nel_pay/registration/token/ConfirmationTokenService.java
+++ src/main/java/com/example/nel_pay/registration/token/ConfirmationTokenService.java
@@ -1,4 +1,15 @@
 package com.example.nel_pay.registration.token;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ConfirmationTokenService {
+
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token){
+        confirmationTokenRepository.save(token);
+    }
 }

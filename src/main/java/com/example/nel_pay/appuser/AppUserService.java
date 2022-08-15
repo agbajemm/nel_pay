9095dfc0dd900c173604549a4d7d754ee79b3faa
agbajemm <agbajemhmd316@gmail.com>
2022-08-15 18:24:46 +0100

    register

--- src/main/java/com/example/nel_pay/appuser/AppUserService.java
+++ src/main/java/com/example/nel_pay/appuser/AppUserService.java
@@ -1,16 +1,11 @@
 package com.example.nel_pay.appuser;
 
-import com.example.nel_pay.registration.token.ConfirmationToken;
-import com.example.nel_pay.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -19,7 +14,6 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -34,16 +28,7 @@ public class AppUserService implements UserDetailsService {
         String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
         appUser.setPassword(encodedPassword);
         appUserRepository.save(appUser);
-
-        String token = UUID.randomUUID().toString();
         // Send confirmation token
-        ConfirmationToken confirmationToken = new ConfirmationToken(
-                token,
-                LocalDateTime.now(),
-                LocalDateTime.now().plusMinutes(15),
-                appUser
-        );
-        confirmationTokenService.saveConfirmationToken(confirmationToken);
-        return token;
+        return "registred";
     }
 }
